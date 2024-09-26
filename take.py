746f99a56258fe97573f2bda7f6650d87e84b859
Dushyant Joshi <dushyantjoshi@Dushyants-MacBook-Air.local>
2024-09-26 17:34:41 +0530

    take.py file added

---
+++ take.py
@@ -0,0 +1,7 @@
+print("git basic")
+# git init
+# git add .
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/dushyantjoshi/data_types_.git
+# git push -u origin main
