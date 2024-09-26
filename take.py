0f69eabfd3520031c829ea67937378f83071876b
Dushyant Joshi <dushyantjoshi@Dushyants-MacBook-Air.local>
2024-09-26 17:49:57 +0530

    new comments made

--- take.py
+++ take.py
@@ -1,1 +1,8 @@
 print("byyy")
+#git basic
+#git init
+#git add .
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin https://github.com/dushyantjoshi/data_types_.git
+#git push -u origin main
