0284c748c9082ca261475747b569cf7b86c79b38
Dushyant Joshi <dushyantjoshi@Dushyants-MacBook-Air.local>
2024-09-29 19:22:18 +0530

    oops concept

--- oops.py
+++ oops.py
@@ -1,22 +1,21 @@
-# def bubble_sort(arr):
-#     n = len(arr)
-#     for pass_no in range(0, n):
-#         for i in range(n-pass_no-1):
-#             if arr[i]>arr[i+1]:
-#                 arr[i], arr[i + 1] = arr[i+1], arr[i]
-#     return arr
-# arr = [3,5,6,3,2,6,7]
-# print(bubble_sort(arr))
+class math():
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    def add(self):
+        return self.a + self.b
+    def __add__(self, other):
+        return self.a + self.b + other.a + other.b
+    def __mul__(self, other):
+        temp_num = self.a * other.a
+        temp_den = self.b * other.b
+        return f"math({temp_num}/{temp_den})"
 
-def selection_sort(arr):
-    n = len(arr)
-    for i in range(n-1):
-        min_index = i
-        for j in range(i+1, n):
-            if arr[j] < arr[min_index]:
-                min_index = j
-        arr[i], arr[min_index] = arr[min_index], arr[i]
-    return arr
-arr = [1,4,6,7,4,2,1,5,6,96]
-print(selection_sort(arr))
-
+    def __str__(self, other):
+        return f"math({self.a}, {self.b})"
+x = math(2, 3)
+print(x.add())
+y = math(3, 4)
+print(y.add())
+print(x+y)
+print(x*y)
