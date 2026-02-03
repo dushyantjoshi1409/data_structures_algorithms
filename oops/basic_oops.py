7617ac57fc472f9b6b72e6a397b04255a2410909
Dushyant Joshi <dj0387@LAP-0546-RMAC.local>
2026-02-03 11:25:28 +0530

    opps relationship and object behaviour

--- oops/basic_oops.py
+++ oops/basic_oops.py
@@ -1,22 +1,26 @@
-
-# You are tasked with designing a class Student that stores and displays information about students.
-# The class must have the following :
-# Attributes :
-# name (string) : Stores the name of the student.
-# rollNumber (int) : Stores the roll number of the student
-# Methods :
-# setDetails (String name, int rollNumber) : This method initializes the attributes name and rollNumber with the values provided by the user.
-# displayDetails() : This method prints the details of the student in following format (The output consist of two separate lines) :
-# Your code goes here
-class Student:
-    def __init__(self):
-        self.name = ""
-        self.rollNumber = 0
-    
-    def setDetails(self, name, rollNumber):
+#Problem 1: Association
+class Driver:
+    def __init__(self, name):
         self.name = name
-        self.rollNumber = rollNumber
-    
-    def displayDetails(self):
-        print("Name :", self.name)
-        print("Roll Number :", self.rollNumber)
+
+class Car:
+    def __init__(self, driver):
+        self.driver = driver
+
+#Problem 2: Aggregation
+class Employee:
+    def __init__(self, name):
+        self.name = name
+
+class Company:
+    def __init__(self, employees):
+        self.employees = employees
+
+#Problem 3: Composition
+class Engine:
+    def __init__(self):
+        print("Engine created")
+
+class Car:
+    def __init__(self):
+        self.engine = Engine()
